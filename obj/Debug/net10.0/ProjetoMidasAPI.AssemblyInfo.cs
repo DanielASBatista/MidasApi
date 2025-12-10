71361316fbf68755c3fcc5daefbbd27a4f28a21f
Andredsonn <andreaesilva6@gmail.com>
2025-12-10 16:59:51 -0300

    ajuste deploy - package

--- obj/Debug/net10.0/ProjetoMidasAPI.AssemblyInfo.cs
+++ obj/Debug/net10.0/ProjetoMidasAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjetoMidasAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04f2fede9722e7ca3c76d4d0da5abe1b04e8f23b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb89b70d196903b5965c7eec16ddfda0a8012101")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjetoMidasAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjetoMidasAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
