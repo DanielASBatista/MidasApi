a280304bdcf8a798b56e034089fc4b198c352260
DanielASBatista <daniel_alves.santos@hotmail.com>
2026-02-11 15:12:37 -0300

    fixed, added and all in between

--- obj/Debug/net10.0/ProjetoMidasAPI.AssemblyInfo.cs
+++ obj/Debug/net10.0/ProjetoMidasAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjetoMidasAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+265aa91a78523dc586083f316ba0712675661c0a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+126dac5eccb8db4d8049f0ec69ca72a2bab2a35b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjetoMidasAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjetoMidasAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
